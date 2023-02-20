2602a71b59bdc78a25adcd5e9b01ec6a1f125a02
dltjsam <dltjsam@gmail.com>
2023-02-20 13:27:34 +0900

    cocktailTest #06

---
+++ src/signature/signatureDetail.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+
+function SignatureDetail() {
+    return (
+        <div>시그니처 디테일</div>
+    )
+}
+export default SignatureDetail;
