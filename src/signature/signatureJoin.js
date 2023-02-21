7b58342907fffaa8d9fcd7aa2249c2979e8d6a4f
dltjsam <dltjsam@gmail.com>
2023-02-21 13:10:38 +0900

    cocktailTest #07

--- src/signature/signatureJoin.js
+++ src/signature/signatureJoin.js
@@ -9,6 +9,29 @@ function SignatureJoin() {
                 <div style={{gridColumn:'3/4', fontWeight:'600', color:'rgb(110, 110, 110)'}}>모여Bar 가이드에 도전하세요</div>
                 <div style={{gridColumn:'3/4', color:'rgb(110, 110, 110)'}}>♥좋아요♥를 많이 받게되면 모여Bar 가이드에 정식 레시피로 등록됩니다. 매력적인 칵테일을 소개해주세요!</div>
             </div>
+            <div className="signature-join-contents">
+                <div>
+                    <h3>재료정보 ▼</h3>
+                </div>
+                <div>
+                    <h3>칵테일 이름 ▼</h3>
+                </div>
+                <div>
+                    <h3>칵테일 영문이름 ▼</h3>
+                </div>
+                <div>
+                    <h3>칵테일 설명 ▼</h3>
+                </div>
+                <div>
+                    <h3>재료 정보 ▼</h3>
+                </div>
+                <div>
+                    <h3>레시피 정보 ▼</h3>
+                </div>
+                <div>
+                    <button>업로드</button>
+                </div>
+            </div>
         </div>
     )
 }
