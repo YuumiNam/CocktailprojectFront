31b463da97433bdacd40df37bb721ef71aaa26cb
dltjsam <dltjsam@gmail.com>
2023-03-15 18:11:37 +0900

    cocktailTest #Map API

---
+++ src/map/map.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+function Map(props) {
+    return (
+        <>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        <div>주변Bar 페이지임</div>
+        </>
+    )
+}
+
+export default Map;
