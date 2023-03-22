d5d71bb627f406202b46785f4cc9bd21025c3a0a
dltjsam <dltjsam@gmail.com>
2023-03-22 10:46:12 +0900

    cocktailTest #검색

--- src/search.js
+++ src/search.js
@@ -27,36 +27,19 @@ function Search(props) {
             <button className="cocktail-btn">시그니처</button>
         </div>
 
-        {/* // <div style={{display:'grid', gridTemplateRows:'1fr 1fr 1fr', paddingLeft:'15%', paddingRight:'15%'}}>
-        //     <div>
-        //         <span>칵테일 ▼</span>
-
-        //     </div>
-        //     <div>
-        //         <span>재료 ▼</span>
-        //     </div>
-        //     <div>
-        //         <span>시그니처 ▼</span>
-        //     </div>
-        // </div>
-        // </> */}
-
-        {/* <> */}
-        <h1> test</h1>
-
-        <div>
-            <h2>칵테일</h2>
-            <div style={{ width: "100%" }}>
+        <div className='search-contatiner'>
+            <div>
+                <h2 style={{marginBottom:'50px'}}>칵테일 ▼</h2>
+                <div className='search-contents-box'>
                 {Cdata.length === 0 ? (
-                    <div style={{ textAlign: "center", border: "1px solid" , minHeight: "200px"}}>
-                        <h2>검색결과 없음</h2>
-
+                    <div className='search-contents-none'>
+                        <h2 style={{paddingTop:'50px'}}>검색결과 없음</h2>
                     </div>
                 ) : (
                     Cdata.map((x, i) => {
                         return (
                             <Link to={`/cocktail/${x.no}`} key={i}>
-                                <div style={{ cursor: "pointer", border: "1px solid" }}>
+                                <div style={{ cursor: "pointer" }}>
                                     <img src={x.cocktailImages[0].url} width='280px' height='200px' style={{ borderRadius: '10px' }} alt="cocktail"></img>
                                     <div className='signature-contents' style={{ fontWeight: '800', padding: '10px 0px' }}>{x.name}</div>
                                     <div className='signature-contents' style={{ color: 'rgb(131, 131, 131)', fontSize: '12px' }}>{x.cocktailContents}</div>
@@ -66,19 +49,20 @@ function Search(props) {
                     })
                 )}
             </div>
-        </div>
-        <div>
-            <h2>재료</h2>
-            <div style={{ width: "100%" }}>
-                {Idata.length == 0 ? (
-                    <div style={{ textAlign: "center", border: "1px solid" , minHeight: "200px"}}>
-                        <h2>검색결과 없음</h2>
+            </div>
+
+            <div>
+                <h2 style={{marginBottom:'50px'}}>재료 ▼</h2>
+                <div className='search-contents-box'>
+                {Idata.length === 0 ? (
+                    <div className='search-contents-none'>
+                        <h2 style={{paddingTop:'50px'}}>검색결과 없음</h2>
                     </div>
                 ) : (
                     Idata.map(x => {
                         return (
                             <Link to={`/cocktail/${x.no}`}>
-                                <div style={{ cursor: "pointer", border: "1px solid" }}>
+                                <div style={{ cursor: "pointer"}}>
                                     <img src={x.image} width='280px' height='200px' style={{ borderRadius: '10px' }} alt="ingredient"></img>
                                     <div className='signature-contents' style={{ fontWeight: '800', padding: '10px 0px' }}>{x.name}</div>
                                     <div className='signature-contents' style={{ color: 'rgb(131, 131, 131)', fontSize: '12px' }}>{x.contents}</div>
@@ -87,6 +71,7 @@ function Search(props) {
                         )
                     })
                 )}
+                </div>
             </div>
         </div>
         </>
